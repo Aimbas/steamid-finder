65accafba85d1107ec7a9c3f4ef88b6ce68ddd47
Aimbass <facuu_arana@hotmail.com>
2024-03-28 12:23:04 -0300

    Logic change

--- app/components/Info/index.js
+++ app/components/Info/index.js
@@ -3,51 +3,37 @@ import { useEffect, useState } from "react";
 
 export default function Info(){
   const regex = /(?<=\/id\/)\d+/
-  const [formData, setFormData] = useState(null)
-  const [fetchData, setFetchData] = useState(null);
+  const [steamIds, setSteamIds] = useState(null)
 
-    function get_form_data(){
-      console.log("SE LLAMA")
-      const formulario = document.getElementById('profile_form')
 
-      if(formulario){
-        formulario.querySelectorAll('input').forEach(input => {
-          const matches = input.value.match(regex)
-          if (matches){
-            setFormData(matches[0])
-            return
-          }
-        });
-      } 
+    useEffect(() => {
+      console.log(steamIds)
+    }, [steamIds])
+
+    const get_steamid = async (id) => {        
+ 
+        const response = await fetch(`http://localhost:3001/get_steam_id?id=${id}`);
+        const newData = await response.json();
+        setSteamIds(newData)
+
     }
 
+    const handleSubmit = (e) => {
+      e.preventDefault();
 
-   
+      const data = new FormData(e.target)
 
-    // useEffect(() => {
-    //   console.log("Fetch data: ", fetchData)
-    // }, [fetchData])
-    
-    useEffect(() => { 
-      console.log("Form Data:", formData)
-
-      if(formData != null){
-
-        const fetchData2 = async () => {
-
-
-            const response = await fetch(`http://localhost:3001/get_steam_id?id=${formData}`);
-            const newData = await response.json();
-            
-            setFetchData(newData)
+        if (data.has("profile_input")) {
+          const matches = data.get("profile_input").match(regex);
+          if(matches){
+            get_steamid(matches[0])
           }
+        } 
+      
+    }
+    
 
-      
-        fetchData2();
-      }
-      
-      
-    }, [formData])
+  
 
 
     
@@ -61,13 +47,13 @@ export default function Info(){
                 <p className="text-sm text-gray-400 ">2. Copy and paste the link in the SteamID search bar</p>
                 <p className="text-sm text-gray-400 ">3. Send your profile and wait</p>
               </div>
-              <form onSubmit={get_form_data} id="profile_form" name="profile_form" className="flex w-full justify-center items-center p-6">
+              <form onSubmit={handleSubmit} id="profile_form" name="profile_form" className="flex w-full justify-center items-center p-6">
                 <input type="url" className="w-full sm:w-3/4 px-2 py-1 xl:w-1/2 text-sm text-center rounded-md" placeholder="www.steam.com/profile"	 id="profile_input" name="profile_input" required minLength="4" size="10" />
               </form>
             </div>
       </section>
       <section className="flex h-full w-full p-1 sm:h-3/4 ">
-          {!fetchData ? ( 
+          {!steamIds ? ( 
             <div className="flex w-full h-full border-double border-4 border-gray-600 backdrop-blur-xl">
                 <div className="flex w-full items-center justify-center">
                   <p className="text-white text-opacity-35">Awaiting profile...</p>
@@ -76,7 +62,7 @@ export default function Info(){
           ) : (
             <div className="flex w-full h-full border-double border-4  border-gray-600 backdrop-blur-xl">
               <div className="flex w-full items-center justify-center">
-                <p className="text-white text-opacity-35">STEAMID: </p>
+                <p className="text-white text-opacity-35">STEAMID: {steamIds.steamid}</p>
               </div>
             </div>
           )}
