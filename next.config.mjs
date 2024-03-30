5ddc548095a27033dd693b0a6d34da740efde9d9
Aimbass <facuu_arana@hotmail.com>
2024-03-30 07:23:27 -0300

    Next config to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export", 
+    basePath: "/steamid-finder",
+    images: {
+        unoptimized: true,
+      },
+  };
 
 export default nextConfig;
+
